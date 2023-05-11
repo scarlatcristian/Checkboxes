ed60bb191900f0e895b95b5e7a0c3a06a708e0dc
cristi <scarlat.cristian.gabriel@gmail.com>
2023-05-11 04:10:24 +0300

    added JS

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const toggles = document.querySelectorAll(".toggle");
+const good = document.querySelector("#good");
+const cheap = document.querySelector("#cheap");
+const fast = document.querySelector("#fast");
+
+const doTheTrick = (theClickedOne) => {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) fast.checked = false;
+    if (cheap === theClickedOne) good.checked = false;
+    if (fast === theClickedOne) cheap.checked = false;
+  }
+};
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => {
+    doTheTrick(e.target);
+  })
+);
